e76b56f97ae0c42cfb2cc2192ae6188180741c05
PatrickBarsch <PatrickGB@gmx.de>
2021-12-09 16:47:44 +0100

    We encrypt passwords

--- models/user.js
+++ models/user.js
@@ -1,12 +1,38 @@
 'use strict';
 
+const bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 10;
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
   email: { type: String, required: true, index: { unique: true } },
   password: { type: String, required: true }
 });
+
+UserSchema.pre('save', function(next) {
+  const user = this;
+  
+  // generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+      if (err) return next(err);
+
+      // hash the password using our new salt
+      bcrypt.hash(user.password, salt, function(err, hash) {
+          if (err) return next(err);
+          // override the cleartext password with the hashed one
+          user.password = hash;
+          next();
+      });
+  });
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+      if (err) return cb(err);
+      cb(null, isMatch);
+  });
+};
 
 const User = mongoose.model('User', UserSchema);
 
--- spec/models/user.spec.js
+++ spec/models/user.spec.js
@@ -13,8 +13,24 @@ describe('User model', function() {
   });
 
   it('user has an email and password property', function() {
-    const user = new User({ email: 'test@test.com', password: '1234' });
+    const user = new User({ email: 'test@test.com', password: 'MY_very_securePWD123!' });
     expect(user.email).toEqual('test@test.com');
-    expect(user.password).toEqual('1234');
+    expect(user.password).toEqual('MY_very_securePWD123!');
   });
+
+  it('Can test if an encrypted password matches', function(done) {
+    const user = new User({ email: 'happy@test.com', password: 'MY_very_securePWD123!' });
+    user.save(function(err) {
+      expect(err).toBeNull();
+      User.find({ email: 'happy@test.com' }, function(err, user) {
+        if (err) throw err; 
+        user[0].comparePassword('MY_very_securePWD123!', function(err, isMatch) {
+          if (err) throw err;
+          expect(isMatch).toBe(true); 
+          done(); 
+        }); 
+      });
+    });
+  }); 
+
 });
