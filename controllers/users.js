d31dfa097cb7220c2810494c50f925309926c8dd
michael steinacher <steinacherm@hotmail.co.uk>
2021-12-14 16:59:14 +0000

    Added user already exists message when user tries to create a user that is in the db

--- controllers/users.js
+++ controllers/users.js
@@ -8,18 +8,27 @@ const UsersController = {
   Create: function(req, res) {
     
     // read default image (paw.png)
-      let img = fs.readFileSync(__dirname + '/../public/images/paw.png')
-      const encodedimage = img.toString('base64')
-      req.body.filename = 'default image'
-      req.body.contentType = 'image/jpeg'
-      req.body.imageBase64 = encodedimage
+    let img = fs.readFileSync(__dirname + '/../public/images/paw.png')
+    const encodedimage = img.toString('base64')
+    req.body.filename = 'default image'
+    req.body.contentType = 'image/jpeg'
+    req.body.imageBase64 = encodedimage
 
-    var user = new User(req.body);
-    user.save(function(err) {
-      if (err) { 
-        throw err; }
-      res.status(201).redirect('/sessions/new');
-    });
+    var email = req.body.email;
+
+    User.findOne({email: email}).then(user => { 
+      if (!user) {  
+        var newUser = new User(req.body);
+        newUser.save(function(err) {
+          if (err) { 
+            throw err; 
+          }
+          res.status(201).redirect('/sessions/new');
+        });
+      } else {
+        res.redirect('/users/new?error=' + encodeURIComponent('Already_Exists'));
+      }
+    })
   },
 }
 
