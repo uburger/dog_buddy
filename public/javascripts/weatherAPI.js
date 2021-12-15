191a8953158d45c9791fc37fe850f426848ce06d
yasmin <yasmin.jones@digital.justice.gov.uk>
2021-12-15 16:48:31 +0000

    xyz

--- public/javascripts/weatherAPI.js
+++ public/javascripts/weatherAPI.js
@@ -1,10 +1,24 @@
-// const { name } = require("ejs"); 
 
-//weather app api
+// import dotenv from 'dotenv'
+// import errorReporter from './errorReporter' 
+// dotenv.config()
+// errorReporter.client.report(new Error('faq example'))
+
+if(process.env.WEATHER_API_KEY === 'debug'){
+    setDebugLevel(1)
+}
+
+
+require('dotenv').config()
+dotenv.config()
+console.log(process.env.WEATHER_API_KEY)
+var API_KEY = process.env.WEATHER_API_KEY; 
+
+
 var button = document.querySelector('.button')
 var inputValue = document.querySelector('.inputValue')
 
-// Grabbing the h and p tags from the DOM
+// Grabbing the p tags from the DOM
 var city = document.querySelector('.name');
 var desc = document.querySelector('.desc');
 var temp = document.querySelector('.temp');
@@ -15,16 +29,17 @@ var temp = document.querySelector('.temp');
 // 2) part 1 - print the api json data - console.log(data). part 2 - grab the json data from api call
 // 3) Create error message if api call does not work 
 button.addEventListener('click', function(){
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=bdb754e7a2357a9c9a27bb7f367b3cd3')
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid='+API_KEY)
     .then(response => response.json())
     .then(data => {
-        var nameValue = data['name']; //key value pairsfrom Json data
+        var nameValue = data['name']; //key value pairs from Json data 
         var tempValue = data['main']['temp']; 
         var descValue = data['weather'][0]['description']; //data was inside an array
-        
+       
         city.innerHTML =nameValue;
         temp.innerHTML =tempValue;
         desc.innerHTML =descValue;
     })
 .catch(err => alert("Wrong city name, please try again"))
 })
+
