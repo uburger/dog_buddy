a69425056470ea59fc86b234e72891b8d207c2b3
yasmin <yasmin.jones@digital.justice.gov.uk>
2021-12-15 14:29:46 +0000

    chat now working

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-const io = require('socket.io')(http)
+const io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
-    res.render('./chat/index.ejs');;
+    res.render('./chat/index.ejs');
 });
 
-const server = http.listen(8080, function() {
+ http.listen(8080, function() {
     console.log('listening on *:8080');
 });
+
 
 io.sockets.on('connection', function(socket) {
     socket.on('username', function(username) {
         socket.username = username;
-        io.emit('is_online', '🔵 <i>' + socket.username + ' joined the chat..</i>');
+        io.emit('is_online', '🔵 <i>' + socket.username + ' join the chat..</i>');
     });
 
-    socket.on('disconnect', function(username) {
+    socket.on('disconnect', function() {
         io.emit('is_online', '🔴 <i>' + socket.username + ' left the chat..</i>');
     })
 
