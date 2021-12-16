9ff25dd39cbd69620b123e42b83cc7fc3c16b552
yasmin <yasmin.jones@digital.justice.gov.uk>
2021-12-16 10:16:44 +0000

    weather api is working and key is hidden

--- bundle.js
+++ bundle.js
@@ -1,4 +1,19 @@
-import WEATHER_API_KEY from "./apiKey.js"; 
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+const WEATHER_API_KEY = "bdb754e7a2357a9c9a27bb7f367b3cd3";
+
+// export default WEATHER_API_KEY; 
+
+module.exports = WEATHER_API_KEY;
+},{}],2:[function(require,module,exports){
+// import WEATHER_API_KEY from "./apiKey.mjs"; 
+
+const WEATHER_API_KEY = require('./apiKey.js');
+
+// require('dotenv').config()
+// dotenv.config()
+// console.log(process.env.WEATHER_API_KEY)
+// var API_KEY = process.env.WEATHER_API_KEY; 
+
 
 var button = document.querySelector('.button')
 var inputValue = document.querySelector('.inputValue')
@@ -28,3 +43,5 @@ button.addEventListener('click', function(){
 .catch(err => alert("Wrong city name, please try again"))
 })
 
+
+},{"./apiKey.js":1}]},{},[2]);
